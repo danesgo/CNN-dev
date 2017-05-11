c4f897658bae2d84b50cf245e9160069eefe6d52
danesgo <degs432@hotmail.com>
2017-05-11 11:47:40 -0600

    added central python script

---
+++ cnn-mnist.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+#Imports
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.contrib import learn
+from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+#Our application logic will be added here
+
+if __name__ == "__main__": #nota that necessarily
+tf.app.run()
